dfb8804163971e28a17dc9576f7236919a628913
greg <gsgrebe@gmail.com>
2017-01-05 17:58:55 -0500

    removing boost and adding it to install script

--- install.sh
+++ install.sh
@@ -13,11 +13,15 @@ git submodule update
 # Install Boost Libraries
 # ------------------------
 # requires gcc 4.8 ... We are just going to assume we have it
-# untar boost
-tar -xzf "$RR_DIR/boost_1_60_0.tar.gz" -C $RR_DIR
-BOOST_DIR="$RR_DIR/boost_1_60_0"
+# download boost
+wget "http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz"
+# untar
+tar -xzf "boost_1_60_0.tar.gz"
+# remove the tarball
+rm "boost_1_60_0.tar.gz"
 
 # Install Boost
+BOOST_DIR="$RR_DIR/boost_1_60_0"
 cd $BOOST_DIR
 ./bootstrap
 ./b2 --with-date time --with-thread --with-system --with-regex --with-filesystem --with-chrono --with-atomic \
