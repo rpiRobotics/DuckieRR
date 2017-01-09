757f8c30e5afe8df5e929d24870469ceb15c965a
greg <gsgrebe@gmail.com>
2017-01-09 12:14:09 -0500

    fix the setup.py... somehow values got hard coded

--- drive/include/adafruit_drivers/setup.py
+++ drive/include/adafruit_drivers/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(name='adafruit_drivers',
 	version='0.0.0',
-	package_dir={'': '/home/greg/duckiebot_ws/DuckieRR/drive/include/adafruit_drivers/include'},
+	package_dir={'': '${CMAKE_CURRENT_SOURCE_DIR}/include'},
 	packages=['Adafruit_ADS1x15', 'Adafruit_GPIO','Adafruit_I2C','Adafruit_MotorHAT','Adafruit_PWM_Servo_Driver'],
+	install_requires=['smbus']
 	)
--- drive/include/dagu_car/setup.py
+++ drive/include/dagu_car/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(name='dagu_car',
 	version='0.0.0',
-	package_dir={'': '/home/greg/duckiebot_ws/DuckieRR/drive/include/dagu_car/include'},
+	package_dir={'': '${CMAKE_CURRENT_SOURCE_DIR}/include'},
 	packages=['dagu_car'],
 	install_requires=['adafruit_drivers'])
