ba3170df52e24ca5c3562ca006cda36ef416bf57
Greg <gsgrebe@gmail.com>
2017-03-08 21:39:38 +0000

    changing to Tustin and trying out other way of calculating gains

--- clients/DriveByAngle/DriveByAngle_onDuckie.py
+++ clients/DriveByAngle/DriveByAngle_onDuckie.py
@@ -12,6 +12,9 @@ import thread
 # Define a bunch of globals
 nodetect_limit = 15
 
+wn_divide = 5
+zeta = 10
+
 acc_min = -1.0
 acc_max = 1.0
 
@@ -231,16 +234,17 @@ def run_main_loop():
                 break
 
         # compute the derivative
-        alpha_dot = (alpha-alpha_prev)/ifs
-        
-        # keep the last 5 values
-        alpha_dot_list[framenum%5] = alpha_dot
+        #alpha_dot = (alpha-alpha_prev)/ifs
+        alpha_dot = (2./ifs)*(alpha-alpha_prev)/(alpha+alpha_prev)
 
-        if framenum < 4:
-            alpha_dot_avg = alpha_dot
-        else:
+        # keep the last 5 values
+        #alpha_dot_list[framenum%5] = alpha_dot
+
+        #if framenum < 4:
+        #    alpha_dot_avg = alpha_dot
+        #else:
             # implement a moving average
-            alpha_dot_avg = np.mean(alpha_dot_list)
+        #    alpha_dot_avg = np.mean(alpha_dot_list)
 
         # save the last value
         alpha_prev = alpha 
@@ -248,7 +252,7 @@ def run_main_loop():
         # Determine controller action
         ang_err = (1.0/alpha)-(1.0/alpha_d)
 
-        acc = Kp*ang_err + Kd*alpha_dot_avg
+        acc = Kp*ang_err + Kd*alpha_dot
         acc = np.clip(acc,acc_min,acc_max)
 
         vel += acc*ifs
@@ -280,13 +284,13 @@ def run_main_loop():
         
     print "Average Loop Freq: %f"%(1.0/avg_time)
     # write data to log file
-    import csv 
+    import csv
+    data = {'time': log_time, 'acc': log_acc, 'vel': log_vel, 'omg':log_omg} 
     filename = time.strftime("%Y-%m-%d-%H-%M-%S") + '.csv'
     with open(filename, 'wb') as myfile:
-        fieldnames = ['time','acc','vel','omg']
-        wr = csv.DictWriter(myfile,fieldnames)
-        wr.writeheader()
-        wr.writerows({'time': log_time, 'acc': log_acc, 'vel': log_vel, 'omg':log_omg})
+        wr = csv.writer(myfile)
+        wr.writerow(data.keys())
+        wr.writerows(zip(*data.values()))
 
 
 
@@ -343,12 +347,16 @@ if __name__ == '__main__':
     drive = RRN.ConnectService("rr+local:///?nodename=Duckiebot.Drive&service=Drive")
             
     vel_max = drive.limit
-    drive.trim = -0.02
+    #drive.trim = -0.03
     drive.gain = 2.0
 
     # Show detection and capture the desired distance
     alpha_d = get_initial_detection()
-    
+   
+    Kp = (2*np.pi*framerate/wn_divide)**2
+    Kd = -2*zeta*np.sqrt(Kp)/alpha_d**2 
+    print Kp
+    print Kd
     # Run the main loop
     run_main_loop()
 
